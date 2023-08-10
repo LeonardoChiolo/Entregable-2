a9f921661fbfa29014db24e6473de3ca58436dc2
unknown <leochiolo@gmail.com>
2023-08-10 19:11:18 -0300

    Location

---
+++ src/constants/firebase/index.js
@@ -0,0 +1,6 @@
+export const URL_BASE_FIREBASE_REALTIME_DATABASE =
+  'https://coolstore-95bb9-default-rtdb.firebaseio.com';
+export const FIREBASE_API_KEY = 'AIzaSyA1DVVWZ_0WinvdSH8pxsEwSGmVYOdwlQ8';
+export const FIREBASE_AUTH_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts';
+export const FIREBASE_AUTH_SIGN_IN_API_URL = `:signInWithPassword?key=${FIREBASE_API_KEY}`;
+export const FIREBAS_AUTH_SIGN_UP_API_URL = `:signUp?key=${FIREBASE_API_KEY}`;
